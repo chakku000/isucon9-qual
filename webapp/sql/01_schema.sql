698f1386e53d6ca3754a5ee99edcf4f804fe29d7
45deg <45deg@users.noreply.github.com>
2019-09-08 12:10:24 +0900

    1210

--- webapp/sql/01_schema.sql
+++ webapp/sql/01_schema.sql
@@ -30,10 +30,10 @@ CREATE TABLE `items` (
   `category_id` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  INDEX idx_category_id (`category_id`)
-  INDEX idx_status (`status`)
-  INDEX idx_created_at (`created_at`)
-  INDEX idx_seller_id (`seller_id`)
+  INDEX idx_category_id (`category_id`),
+  INDEX idx_status (`status`),
+  INDEX idx_created_at (`created_at`),
+  INDEX idx_seller_id (`seller_id`),
   INDEX idx_buyer_id (`buyer_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
